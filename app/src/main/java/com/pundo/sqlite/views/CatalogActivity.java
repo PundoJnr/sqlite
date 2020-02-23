417eea2bb070984ad72293a713b5966ea30079f3
PundoJnr <pundoarthur@gmail.com>
2020-02-23 07:25:03 +0300

    Inflate menu items

--- app/src/main/java/com/pundo/sqlite/views/CatalogActivity.java
+++ app/src/main/java/com/pundo/sqlite/views/CatalogActivity.java
@@ -1,9 +1,15 @@
 package com.pundo.sqlite.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.pundo.sqlite.R;
 
 public class CatalogActivity extends AppCompatActivity {
@@ -12,5 +18,39 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editor_activity);
+
+        //onclick on floatingaction
+
+        FloatingActionButton fb = findViewById(R.id.fab);
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CatalogActivity.this, EditorActivity.class));
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.dots, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.add:
+                //add information
+
+                return true;
+            case R.id.delete:
+                //delete pet information
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
